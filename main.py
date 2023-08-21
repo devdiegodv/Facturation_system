74089b5becc9601f639b33b6f651539dc84f4273
dev-diegov <diegoidv.us@gmail.com>
2023-08-21 18:37:05 -0400

    tkinter logic screen

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ app = Tk()
 # screen size
 app.geometry("1020x630+0+0") #1020 width - 630height - 0x - 0y
 
+# avoid max screen
+app.resizable(False, False) # X and Y position can't be modified
+
+# window tittle
+app.title("Facturation System ®")
+
+# background color screen
+app.config(bg='Gray')
+
 # avoid window to be closed
 app.mainloop()
