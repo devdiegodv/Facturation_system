f3aee72e3864228a2ffc6ed890f965d615a5a36e
dev-diegov <diegoidv.us@gmail.com>
2023-08-21 18:58:04 -0400

    panels logic

--- main.py
+++ main.py
@@ -15,5 +15,49 @@ app.title("Facturation System ®")
 # background color screen
 app.config(bg='Gray')
 
+# top panel
+top_panel = Frame(app, bd=1, relief=FLAT) # bd = border
+top_panel.pack(side=TOP) # place it on top
+
+# title label
+title_label = Label(top_panel, text="Facturation System", fg="azure4", font=("Dosis", 58), bg="burlywood", width=22)
+title_label.grid(row=0, column=0)
+
+# left panel
+left_panel = Frame(app, bd=1, relief=FLAT)
+left_panel.pack(side=LEFT)
+
+# costs panel
+costs_panel = Frame(left_panel, bd=1, relief=FLAT)
+costs_panel.pack(side=BOTTOM)
+
+# food panel
+food_panel = LabelFrame(left_panel, text="Food", font=("Dosis", 19, "bold"), bd=1, relief=FLAT, fg="azure4")
+food_panel.pack(side=LEFT)
+
+# drinks panel
+drinks_panel = LabelFrame(left_panel, text="Food", font=("Dosis", 19, "bold"), bd=1, relief=FLAT, fg="azure4")
+drinks_panel.pack(side=LEFT)
+
+# desserts panel
+desserts_panel = LabelFrame(left_panel, text="Food", font=("Dosis", 19, "bold"), bd=1, relief=FLAT, fg="azure4")
+desserts_panel.pack(side=LEFT)
+
+# right panel
+right_panel = Frame(app, bd=1, relief=FLAT)
+right_panel.pack(side=RIGHT)
+
+# calculator panel
+calculator_panel = Frame(right_panel, bd=1, relief=FLAT, bg="burlywood")
+calculator_panel.pack()
+
+# receipt panel
+receipt_panel = Frame(right_panel, bd=1, relief=FLAT, bg="burlywood")
+receipt_panel.pack()
+
+# buttons panel
+buttons_panel = Frame(right_panel, bd=1, relief=FLAT, bg="burlywood")
+buttons_panel.pack()
+
 # avoid window to be closed
 app.mainloop()
